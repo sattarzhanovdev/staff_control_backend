1014b918c094ec52af18b6f14404208116a380d0
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-05-02 21:36:21 +0600

    push

--- staff/views.py
+++ staff/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from django.utils import timezone
+from datetime import timedelta
 
 from .models import Работник, Посещаемость, Бонус, Задача, Расходы
 from .serializers import (
