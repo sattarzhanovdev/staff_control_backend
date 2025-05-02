c189bc7cfc0c02769a615a1bc1b1d3a0a5e3e570
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-05-02 20:36:18 +0600

    final update

--- staff/models.py
+++ staff/models.py
@@ -100,8 +100,7 @@ class Расходы(models.Model):
 
     название = models.CharField(max_length=255)
     дата = models.DateTimeField()
-    время = models.TimeField()
-    категория = models.CharField(max_length=10, choices=КАТЕГОРИЯ, default='средний')
+    категория = models.CharField(max_length=20, choices=КАТЕГОРИЯ, default='закуп')
     сумма = models.IntegerField()
 
     def __str__(self):
