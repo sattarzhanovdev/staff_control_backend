d928b24e6de367991a754c55026a0dbde767e822
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-05-02 20:37:48 +0600

    final update

--- staff/admin.py
+++ staff/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Работник, Посещаемость, Бонус, Задача
+from .models import Работник, Посещаемость, Бонус, Задача, Расходы
 
 admin.site.register(Работник)
 admin.site.register(Посещаемость)
 admin.site.register(Бонус)
 admin.site.register(Задача)
+admin.site.register(Расходы)
